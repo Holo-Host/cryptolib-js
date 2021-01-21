211aca348e40d0417616bac353855fb9d163ca63
jetttech <lisa.jetton@holo.host>
2021-01-21 10:57:58 -0600

    update tests

--- tests/test_codec.js
+++ tests/test_codec.js
@@ -3,98 +3,80 @@ const expect						= require('chai').expect;
 const { Codec }						= require('../src/index.js');
 
 describe("Codec.AgentId", () => {
-	it("should decode HoloHash agent ID into HoloHash agent pubkey buffer", async () => {
+    it("should encode public key buffer into HoloHash agent ID and decode it back to a public key buffer", () => {
+		const publicKeyb64 				= "od6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHE";
+		const publicKeyBuffer           =  Buffer.from(publicKeyb64, "base64");
 		const agentId					= "uhCAkod6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHGQCkSp";
+        expect( Codec.AgentId.encode(publicKeyBuffer)		).to.equal(agentId);
+        expect( Codec.AgentId.decode(agentId)		).to.deep.equal(publicKeyBuffer);
+    });
+    
+    it("should encode public key buffer into HoloHash agent ID and decode it to an agent HoloHash buffer", async () => {
+        const publicKeyb64 				= "od6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHE";
+        const publicKeyBuffer           =  Buffer.from(publicKeyb64, "base64");
+        const agentId					= "uhCAkod6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHGQCkSp";
 		const agentHoloHashb64			= "hCAkod6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHGQCkSp";
-		const holohashPubKeyBuffer            = Buffer.from(agentHoloHashb64, "base64" );
+		const holohashPubKeyBuffer      = Buffer.from(agentHoloHashb64, "base64" );
 
+        expect( Codec.AgentId.encode(publicKeyBuffer)		).to.equal(agentId);
 		expect( Codec.AgentId.decodeToHoloHash(agentId)).to.deep.equal(holohashPubKeyBuffer);
-	});
-
-	it("should decode HoloHash agent ID into public key buffer", () => {
-		const agentId					= "uhCAkod6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHGQCkSp";
-		const publicKeyb64 				= "od6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHE";
-		const publicKeyBuffer           =  Buffer.from(publicKeyb64, "base64");
-
-		expect( Codec.AgentId.decode(agentId)		).to.deep.equal(publicKeyBuffer);
-	});
-
-	it("should encode public key buffer into HoloHash agent ID", () => {
-		const publicKeyb64 				= "od6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHE";
-		const publicKeyBuffer           =  Buffer.from(publicKeyb64, "base64");
-		const agentId					= "uhCAkod6AkumAC8VNFgDHZsdpDBPBGPpPxt2QyxebjY6zfHGQCkSp";
-		expect( Codec.AgentId.encode(publicKeyBuffer)		).to.equal(agentId);
 	});
 });
 
 describe("Codec.Base36", () => {
-	it("should decode agent ID using base36", async () => {
+	it("should decode agent ID using base36 and then encode agent ID back into base36", async () => {
 		const urlAgentId				= "wjzlh5yt3uk0mzpcor0i12ol0rrpxdydzggt4b2fvr8yealc";
 		const publicKey					= Buffer.from("AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyA=", "base64");
 
-		expect( Codec.Base36.encode(publicKey)		).to.equal(urlAgentId);
-	});
-
-	it("should encode agent ID into base36", async () => {
-		const publicKey					= Buffer.from("AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyA=", "base64");
-		const urlAgentId				= "wjzlh5yt3uk0mzpcor0i12ol0rrpxdydzggt4b2fvr8yealc";
-
-		expect( Codec.Base36.encode(publicKey)		).to.equal(urlAgentId);
+        expect( Codec.Base36.encode(publicKey)		).to.equal(urlAgentId);
+        expect( Codec.Base36.encode(publicKey)		).to.equal(urlAgentId);
 	});
 });
 
 describe("Codec.Signature", () => {
-	it("should decode Signature string into message bytes", async () => {
+	it("should decode Signature string into message bytes and then encode message bytes back into Signature string", async () => {
 		const messageBytes				= Buffer.from("example 1");
 		const base64String				= "ZXhhbXBsZSAx";
 
-		expect( Codec.Signature.decode(base64String)	).to.deep.equal(messageBytes)
-	});
-
-	it("should encode message bytes into Signature string", async () => {
-		const base64String				= "ZXhhbXBsZSAy";
-		const messageBytes				= Buffer.from("example 2");
-
-		expect( Codec.Signature.encode(messageBytes)	).to.equal(base64String);
+        expect( Codec.Signature.decode(base64String)	).to.deep.equal(messageBytes);
+        expect( Codec.Signature.encode(messageBytes)	).to.equal(base64String);
 	});
 });
 
 describe("Codec.HoloHash", () => {
-	it("should return holohash buffer from raw buffer", () => {
+
+	it("should encode raw buffer into HoloHash string and then decode HoloHash string into raw buffer", async () => {
+		const hashString				= "uhCEkWCsAgoKkkfwyJAglj30xX_GLLV-3BXuFy436a2SqpcEwyBzm";
+		const rawBuffer            	=  Buffer.from( "WCsAgoKkkfwyJAglj30xX/GLLV+3BXuFy436a2SqpcE=", "base64" );
+
+        expect( Codec.HoloHash.encode('entry', rawBuffer)).to.equal(hashString);
+		expect( Codec.HoloHash.decode(hashString)).to.deep.equal(rawBuffer);
+	});
+
+    it("should encode holohash buffer into HoloHash string and then decode HoloHash string into raw buffer", async () => {
+        const holoHashBuffer   			= Buffer.from("hCEkWCsAgoKkkfwyJAglj30xX_GLLV-3BXuFy436a2SqpcEwyBzm", "base64");
+        const hashString				=  "uhCEkWCsAgoKkkfwyJAglj30xX_GLLV-3BXuFy436a2SqpcEwyBzm";
+        const rawBuffer            	=  Buffer.from( "WCsAgoKkkfwyJAglj30xX/GLLV+3BXuFy436a2SqpcE=", "base64" );
+
+        expect( Codec.HoloHash.encode('entry', holoHashBuffer)).to.equal(hashString);
+        expect( Codec.HoloHash.decode(hashString)).to.deep.equal(rawBuffer);
+    });
+
+    it("should return holohash buffer from raw buffer", () => {
 		const dataBuffer            	=  Buffer.from("WCsAgoKkkfwyJAglj30xX/GLLV+3BXuFy436a2SqpcE=", "base64");
 		const holoHashBuffer            =  Buffer.from("hCEkWCsAgoKkkfwyJAglj30xX/GLLV+3BXuFy436a2SqpcEwyBzm", "base64");
 
 		expect( Codec.HoloHash.holoHashFromBuffer('entry', dataBuffer)	).to.deep.equal(holoHashBuffer);
 	});
-
-	it("should decode HoloHash string into raw buffer", async () => {
-		const hashString				= "uhCEkWCsAgoKkkfwyJAglj30xX_GLLV-3BXuFy436a2SqpcEwyBzm";
-		const hashBuffer            	=  Buffer.from( "WCsAgoKkkfwyJAglj30xX/GLLV+3BXuFy436a2SqpcE=", "base64" );
-
-		expect( Codec.HoloHash.decode(hashString)).to.deep.equal(hashBuffer);
-	});
-
-	it("should encode raw buffer into HoloHash string", async () => {
-		const rawBuffer 				= Buffer.from("WCsAgoKkkfwyJAglj30xX/GLLV+3BXuFy436a2SqpcE=", "base64");
-		const hashString				=  "uhCEkWCsAgoKkkfwyJAglj30xX_GLLV-3BXuFy436a2SqpcEwyBzm";
-
-		expect( Codec.HoloHash.encode('entry', rawBuffer)).to.equal(hashString);
-	});
-
-	it("should encode holohash buffer into HoloHash string", async () => {
-		const holoHashBuffer   			= Buffer.from("hCEkWCsAgoKkkfwyJAglj30xX_GLLV-3BXuFy436a2SqpcEwyBzm", "base64");
-		const hashString				=  "uhCEkWCsAgoKkkfwyJAglj30xX_GLLV-3BXuFy436a2SqpcEwyBzm";
-
-		expect( Codec.HoloHash.encode('entry', holoHashBuffer)).to.equal(hashString);
-	});
 });
 
 describe("Codec.Digest", () => {
-	it("should decode SHA-256 multihash into SHA-256 digest buffer", async () => {
-		const hashString				=  "EnV7InN0ZXBzIjp7ImJhc2UiOjY0LCJwcm9jZXNzIjpbImRhdGEgd2lsbCBiZSBoYXNoZWQgaW50byBhIiwic2hhMjU2IG11bHRpaGFzaCB0aGVuIiwiZW5jb2RlZCJdfSwidGVzdCI6ImluZm9ybWF0aW9uIn0=";
+	it("should encode sha256 digest buffer into base64 encoded SHA-256 multihash and then decode SHA-256 multihash into SHA-256 digest buffer", async () => {
 		const hashDigest				= "eyJzdGVwcyI6eyJiYXNlIjo2NCwicHJvY2VzcyI6WyJkYXRhIHdpbGwgYmUgaGFzaGVkIGludG8gYSIsInNoYTI1NiBtdWx0aWhhc2ggdGhlbiIsImVuY29kZWQiXX0sInRlc3QiOiJpbmZvcm1hdGlvbiJ9";
 		const digestBuffer            	=  Buffer.from(hashDigest, "base64");
-		expect( Codec.Digest.decode(hashString)).to.deep.equal(digestBuffer);
+		const hashString				=  "EnV7InN0ZXBzIjp7ImJhc2UiOjY0LCJwcm9jZXNzIjpbImRhdGEgd2lsbCBiZSBoYXNoZWQgaW50byBhIiwic2hhMjU2IG11bHRpaGFzaCB0aGVuIiwiZW5jb2RlZCJdfSwidGVzdCI6ImluZm9ybWF0aW9uIn0=";
+        expect( Codec.Digest.encode(digestBuffer)).to.equal(hashString);
+        expect( Codec.Digest.decode(hashString)).to.deep.equal(digestBuffer);
 	});
 
 	it("should encode stringified data into base64 encoded SHA-256 multihash", async () => {
@@ -109,13 +91,5 @@ describe("Codec.Digest", () => {
 		const hashString				=  "EnV7InN0ZXBzIjp7ImJhc2UiOjY0LCJwcm9jZXNzIjpbImRhdGEgd2lsbCBiZSBoYXNoZWQgaW50byBhIiwic2hhMjU2IG11bHRpaGFzaCB0aGVuIiwiZW5jb2RlZCJdfSwidGVzdCI6ImluZm9ybWF0aW9uIn0=";
 		
 		expect( Codec.Digest.encode(jsonData)).to.equal(hashString);
-	});
-
-	it("should encode sha256 digest buffer into base64 encoded SHA-256 multihash", async () => {
-		const hashDigest					= "eyJzdGVwcyI6eyJiYXNlIjo2NCwicHJvY2VzcyI6WyJkYXRhIHdpbGwgYmUgaGFzaGVkIGludG8gYSIsInNoYTI1NiBtdWx0aWhhc2ggdGhlbiIsImVuY29kZWQiXX0sInRlc3QiOiJpbmZvcm1hdGlvbiJ9";
-		const digestBuffer           	 	=  Buffer.from(hashDigest, "base64");
-		const hashString					=  "EnV7InN0ZXBzIjp7ImJhc2UiOjY0LCJwcm9jZXNzIjpbImRhdGEgd2lsbCBiZSBoYXNoZWQgaW50byBhIiwic2hhMjU2IG11bHRpaGFzaCB0aGVuIiwiZW5jb2RlZCJdfSwidGVzdCI6ImluZm9ybWF0aW9uIn0=";
-		
-		expect( Codec.Digest.encode(digestBuffer)).to.equal(hashString);
 	});
 });
